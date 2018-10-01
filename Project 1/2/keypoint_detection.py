bf9592f9dd57bb3b52a9063b9b7bfff38957a88b
ameenkhan07 <ameenkhan07@gmail.com>
2018-10-01 14:12:18 -0400

    Format files per yapf

--- Project 1/2/keypoint_detection.py
+++ Project 1/2/keypoint_detection.py
@@ -27,7 +27,7 @@ class ScaleSpace:
         self.k = math.sqrt(2)
         self.gauss_pyramid = []  # array of all octaves
         self.dog = []  # array of all DoGs of all octaves
-        self.dog_extrema = [4*[]]
+        self.dog_extrema = [4 * []]
         self.img = cv2.imread("./task2.jpg", 0)
 
     def _show_img(self, img, name='IMAGE'):
@@ -92,7 +92,8 @@ class ScaleSpace:
 
         for i, octave in enumerate(self.dog):
             for im, scale in enumerate(octave):
-                cv2.imwrite("DOG_"+str(i)+"_"+str(im)+".png", np.asarray(scale, dtype = np.uint8))
+                cv2.imwrite("DOG_" + str(i) + "_" + str(im) + ".png",
+                            np.asarray(scale, dtype=np.uint8))
                 # self._show_img(im)
 
     def _detect_keypoints(self):
@@ -101,21 +102,20 @@ class ScaleSpace:
         # first_octave = self.dog[0]
         for o, octave in enumerate(self.dog):
             for s, scale in enumerate(octave):
-                if s in [1,2]:
+                if s in [1, 2]:
                     DX, DY = scale.shape[0], scale.shape[1]
                     temp = [[0 for _ in range(DY)] for _ in range(DX)]
-                    # print(len(temp))
-                    # print(len(temp[1]))
-                    # print(scale.shape, DX, DY)
                     for x, row in enumerate(octave[s], start=0):
                         for y, col in enumerate(row, start=0):
                             # print(row, "X")
-                            neighborhood = get_neigborhood(x,y, scale, octave[s-1], octave[s+1])
-                            if col<min(neighborhood) or col > max(neighborhood):
+                            neighborhood = get_neigborhood(
+                                x, y, scale, octave[s - 1], octave[s + 1])
+                            if col < min(neighborhood) or col > max(
+                                    neighborhood):
                                 temp[x][y] = 255
-                    # self._show_img(scale)
                     self.dog_extrema[0].append(temp)
-                    cv2.imwrite("Extrema_"+str(o)+"_"+str(s)+".png", np.asarray(temp, dtype = np.uint8))
+                    cv2.imwrite("Extrema_" + str(o) + "_" + str(s) + ".png",
+                                np.asarray(temp, dtype=np.uint8))
                     # self._show_img(np.asarray(temp, dtype = np.uint8))
 
 
--- Project 1/2/utils.py
+++ Project 1/2/utils.py
@@ -1,12 +1,12 @@
-
 # Gaussian Filter
 def _gaussian_func(x, y, sig):
     """Gaussian Function used in defining kernel
     """
     pi = 3.14
     num = -(x**2 + y**2)
-    e = np.exp( num / ( 2.0 * sig**2 ) )
-    return (1 /( (2*pi) * sig**2)) * e
+    e = np.exp(num / (2.0 * sig**2))
+    return (1 / ((2 * pi) * sig**2)) * e
+
 
 def _gaussian_kernel(sig):
     """
@@ -14,21 +14,23 @@ def _gaussian_kernel(sig):
     """
     # print(_gaussian_func(-2, 2, 1/math.sqrt(2)))
     kernel = [[0 for x in range(7)] for y in range(7)]
-    for x in range(-3,3):
-        for y in range(-3,3):
+    for x in range(-3, 3):
+        for y in range(-3, 3):
             kernel[x][y] = _gaussian_func(x, y, sig)
     print(kernel)
     return kernel
+
 
 def _flip(mat):
     """Retruns horizontally and vertically flipped Matrices
     """
     # Horizontally flipped (reverse)
-    mat =  mat[:, ::-1]
+    mat = mat[:, ::-1]
     # Vertically flipped (reverse)
     mat = mat[::-1, ...]
     return mat
-    
+
+
 def _conv(img, kernel):
     """
     """
@@ -37,18 +39,18 @@ def _conv(img, kernel):
     # Flip kernel before convolution
     # img = s_flip(img)
     print(".........")
-    output = np.zeros((dim_y,dim_x), np.uint8)
+    output = np.zeros((dim_y, dim_x), np.uint8)
     print(kernel.shape, img.shape)
     # Convolve img and kernel
-    for x in range(1, dim_y-1):
-        for y in range(1, dim_x-1):
-            temp_x = abs(sum(map(sum, img[x-1:x+6, y-1:y+6] * kernel)))
-            if temp_x>255: temp_x=255
-            output[x-1, y-1] = temp_x
+    for x in range(1, dim_y - 1):
+        for y in range(1, dim_x - 1):
+            temp_x = abs(sum(map(sum, img[x - 1:x + 6, y - 1:y + 6] * kernel)))
+            if temp_x > 255: temp_x = 255
+            output[x - 1, y - 1] = temp_x
     return output
     # cv2.imshow('Gaussian Images',np.asarray(output))
 
-# Extrema neighborhood
+
 def get_neigborhood(x, y, mat1, mat2, mat3):
     """Return array of elements in the neighborhood
     of the input coordinates (x,y)
@@ -68,13 +70,13 @@ def get_neigborhood(x, y, mat1, mat2, mat3):
     """
     neighbors = []
     X, Y = mat1.shape
-    
+
     neighbor_coord = lambda x, y : [(x_, y_) for x_ in range(x-1, x+2)
-    for y_ in range(y-1, y+2)if ((0 <= x_ <= X-1) and (0 <= y_ <= Y-1) and 
+    for y_ in range(y-1, y+2)if ((0 <= x_ <= X-1) and (0 <= y_ <= Y-1) and
     (x != x_ or y != y_))]
-    
-    neighbors.extend([mat1[x[0]][x[1]] for x in neighbor_coord(x,y)])
-    neighbors.extend([mat2[x[0]][x[1]] for x in neighbor_coord(x,y)])
-    neighbors.extend([mat3[x[0]][x[1]] for x in neighbor_coord(x,y)])
-    
-    return(neighbors)
+
+    neighbors.extend([mat1[x[0]][x[1]] for x in neighbor_coord(x, y)])
+    neighbors.extend([mat2[x[0]][x[1]] for x in neighbor_coord(x, y)])
+    neighbors.extend([mat3[x[0]][x[1]] for x in neighbor_coord(x, y)])
+
+    return (neighbors)
