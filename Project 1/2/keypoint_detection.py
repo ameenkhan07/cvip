e9372d10f9c447d4b94c8ade61e59cfdaa73aa8f
ameenkhan07 <ameenkhan07@gmail.com>
2018-10-03 12:06:10 -0400

    Added save functionality of DoG extremas  superimposed on original images

--- Project 1/2/keypoint_detection.py
+++ Project 1/2/keypoint_detection.py
@@ -16,6 +16,10 @@ import numpy as np
 import scipy.ndimage
 from utils import *
 
+OUTPUT_DIR = "outputs/"
+if not os.path.exists(OUTPUT_DIR):
+    os.makedirs(OUTPUT_DIR)
+
 
 class ScaleSpace:
     """ Building Scale Space
@@ -27,8 +31,9 @@ class ScaleSpace:
         self.k = math.sqrt(2)
         self.gauss_pyramid = []  # array of all octaves
         self.dog = []  # array of all DoGs of all octaves
-        self.dog_extrema = [4 * []]
-        self.img = cv2.imread("./task2.jpg", 0)
+        self.dog_extrema = []
+        self.img = cv2.imread("./task2.jpg", 0)  # Greyscaled Image
+        self.original_img = cv2.imread("./task2.jpg")
 
     def _show_img(self, img, name='IMAGE'):
         """Utility Function to cv2 show images
@@ -92,34 +97,58 @@ class ScaleSpace:
 
         for i, octave in enumerate(self.dog):
             for im, scale in enumerate(octave):
-                cv2.imwrite("DOG_" + str(i) + "_" + str(im) + ".png",
-                            np.asarray(scale, dtype=np.uint8))
+                name = "DOG_" + str(i + 1) + "_" + str(im + 1) + ".png"
+                cv2.imwrite(
+                    os.path.join(OUTPUT_DIR, name),
+                    np.asarray(scale, dtype=np.uint8))
+                # save_img(np.asarray(scale, dtype=np.uint8), name)
                 # self._show_img(im)
 
     def _detect_keypoints(self):
+        """Save images for all extrema for all Octave DoGs
         """
-        """
-        # first_octave = self.dog[0]
         for o, octave in enumerate(self.dog):
+            extremas_ = []
             for s, scale in enumerate(octave):
                 if s in [1, 2]:
                     DX, DY = scale.shape[0], scale.shape[1]
                     temp = [[0 for _ in range(DY)] for _ in range(DX)]
                     for x, row in enumerate(octave[s], start=0):
                         for y, col in enumerate(row, start=0):
-                            # print(row, "X")
                             neighborhood = get_neigborhood(
                                 x, y, scale, octave[s - 1], octave[s + 1])
                             if col < min(neighborhood) or col > max(
                                     neighborhood):
                                 temp[x][y] = 255
-                    self.dog_extrema[0].append(temp)
-                    cv2.imwrite("Extrema_" + str(o) + "_" + str(s) + ".png",
-                                np.asarray(temp, dtype=np.uint8))
-                    # self._show_img(np.asarray(temp, dtype = np.uint8))
+                    extremas_.append(temp)
+                    name = "Extrema_" + str(o + 1) + "_" + str(s + 1) + ".png"
+                    cv2.imwrite(
+                        os.path.join(OUTPUT_DIR, name),
+                        np.asarray(temp, dtype=np.uint8))
+            self.dog_extrema.append(extremas_)
+
+    def _save_keypoint_overlay(self):
+        """
+        """
+        oct_img = self._sample_img(self.original_img)
+
+        # For every sampled image corresponding to a octave
+        for s, s_img in enumerate(oct_img):
+            for o, extrema_img in enumerate(self.dog_extrema[s]):
+                DX, DY = s_img.shape[0], s_img.shape[1]
+                # print(DX, DY)
+                for i in range(DX):
+                    for j in range(DY):
+                        if extrema_img[i][j] == 255:
+                            s_img[i][j] = 255
+                # self._show_img(s_img)
+                name = "Octave_Images_" + str(s + 1) + "_" + str(o +
+                                                                 1) + ".png"
+                cv2.imwrite(os.path.join(OUTPUT_DIR, name), s_img)
 
 
 s = ScaleSpace()
 s._create_gauss_pyramid()
 s._create_diff_of_gauss()
 s._detect_keypoints()
+s._save_keypoint_overlay()
