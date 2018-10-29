21c880ecbdf6b3d810b98a8a9649ec277a9088a8
ameenkhan07 <ameenkhan07@gmail.com>
2018-10-28 23:08:15 -0400

    Slight refactoring, changing keypoint detection method

--- Project 2/Task1/main.py
+++ Project 2/Task1/main.py
@@ -7,7 +7,7 @@ img2_name = "./mountain2.jpg"
 
 
 def _save(filename, img):
-    """
+    """Saves the image with filename in output dir 
     """
     if not os.path.exists(OUTPUT_DIR):
         os.makedirs(OUTPUT_DIR)
@@ -17,7 +17,24 @@ def _save(filename, img):
     cv.imwrite(filename, img)
 
 
-def _extract_SIFT_keypoints(img_1, img_2):
+def _extract_SIFT_keypoints(sift_obj, img1, img2, img1_g, img2_g):
+    """Extract SIFT features(keypoints and descriptors) and 
+    draw the keypoints and return the images
+    """
+    # Get keypoints and descriptor for image 1
+    keypoint_1, descriptor_1 = sift_obj.detectAndCompute(img1_g, None)
+    # Save Keypoint Image for image 1
+    img_keypoint = cv.drawKeypoints(img1_g, keypoint_1, img1)
+    _save('task1sift1.jpg', img_keypoint)
+
+    # Get keypoints and descriptor for image 2
+    keypoint_2, descriptor_2 = sift_obj.detectAndCompute(img2_g, None)
+    # Save Keypoint Image for image 2
+    img_keypoint = cv.drawKeypoints(img2_g, keypoint_2, img1)
+    _save('task1sift2.jpg', img_keypoint)
+
+    return(keypoint_1, descriptor_1, keypoint_2, descriptor_2)
+
     """
     """
     sift_create_obj = cv.xfeatures2d.SIFT_create()
@@ -41,17 +58,11 @@ def _extract_SIFT_keypoints(img_1, img_2):
 
 if __name__ == '__main__':
 
-    img1 = cv.imread(img1_name)
-    img2 = cv.imread(img2_name)
-    print(img1.shape, img2.shape)
+    img1, img1_g = cv.imread(img1_name), cv.imread(img1_name, 0)
+    img2, img2_g = cv.imread(img2_name), cv.imread(img2_name, 0)
 
-    _extract_SIFT_keypoints(img1, img2)
+    sift_obj = cv.xfeatures2d.SIFT_create()
 
-    # gray = cv.cvtColor(img1, cv.COLOR_BGR2GRAY)
-    # sift = cv.xfeatures2d.SIFT_create()
-    # kp = sift.detect(gray, None)
-    # img = cv.drawKeypoints(gray, kp, img1)
-    # # gray2 = cv.cvtColor(img2, cv.COLOR_BGR2GRAY)
+    keypoint_1, descriptor_1, keypoint_2, descriptor_2 = _extract_SIFT_keypoints(
+        sift_obj, img1, img2, img1_g, img2_g)
 
-    # # cv.imwrite('sift_keypoints.jpg', img1)
-    # _save('img1', img)
