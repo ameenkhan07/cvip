e2ef463fa6fc1751d48b4dd38db6407663a1c49f
ameenkhan07 <ameenkhan07@gmail.com>
2018-11-02 18:38:02 -0400

    Kmeans point cluster color and centroid updates added.

--- Project 2/Task3/main.py
+++ Project 2/Task3/main.py
@@ -1,25 +1,13 @@
+import os
 import matplotlib.pyplot as plt
 import numpy as np
 
-UBIT = 'ameenmoh'
-np.random.seed(sum([ord(c) for c in UBIT]))
-
-# Requirements of projects
-points = np.asarray([
-    [5.9, 4.6, 6.2, 4.7, 5.5, 5.0, 4.9, 6.7, 5.1, 6.0],
-    [3.2, 2.9, 2.8, 3.2, 4.2, 3.0, 3.1, 3.1, 3.8, 3.0]
-])
-
-# Centroids
-Mu = {
-    1: [6.2, 3.2],
-    2: [6.6, 3.7],
-    3: [6.5, 3.0]
-}
-Mu_color = {1: 'r', 2: 'g', 3: 'b'}
+OUTPUT_DIR = "outputs/"
+if not os.path.exists(OUTPUT_DIR):
+    os.makedirs(OUTPUT_DIR)
 
 
-def _show_plot(points, Mu, Mu_color, color_map=[]):
+def _show_plot(points, Mu, Mu_color, color_map=[], _scatter=True, _save=True, filename='temp.png'):
     """
     """
     if len(color_map):
@@ -27,15 +15,19 @@ def _show_plot(points, Mu, Mu_color, color_map=[]):
     else:
         k = '1'
     fig = plt.figure(figsize=(5, 5))
-    
-    plt.scatter(points[0], points[1], color=k,
-                marker='^', edgecolor='b')
+
+    if _scatter:
+        plt.scatter(points[0], points[1], facecolor=k,
+                    marker='^', edgecolor='k')
     # Set Centroids
     for i in Mu.keys():
         plt.scatter(*Mu[i], color=Mu_color[i])
     plt.xlim(4, 7)
     plt.ylim(2, 5)
-    plt.show()
+    if _save:
+        plt.savefig(os.path.join(OUTPUT_DIR, filename))
+    else:
+        plt.show()
 
 
 def _assignment(points, Mu, Mu_color):
@@ -62,6 +54,65 @@ def _assignment(points, Mu, Mu_color):
     return (closest, color)
 
 
-_show_plot(points, Mu, Mu_color)
-closest, color_map = _assignment(points, Mu, Mu_color)
-_show_plot(points, Mu, Mu_color, color_map=color_map)
+def _update_Mu(Mu, closest, points):
+    """
+    """
+    # print(points)
+    # print(closest)
+    # print(Mu)
+    new_Mu = {}
+    for i in Mu.keys():
+        x = round(np.mean([points[0][j1]
+                           for j1, j2 in enumerate(closest) if i == j2]), 2)
+        y = round(np.mean([points[1][j1]
+                           for j1, j2 in enumerate(closest) if i == j2]), 2)
+        # print(x,y)
+        new_Mu[i] = [x, y]
+    # print(new_Mu)
+    return new_Mu
+
+
+if __name__ == '__main__':
+    # Cartesian points to be clusters
+    points = np.asarray([
+        [5.9, 4.6, 6.2, 4.7, 5.5, 5.0, 4.9, 6.7, 5.1, 6.0],
+        [3.2, 2.9, 2.8, 3.2, 4.2, 3.0, 3.1, 3.1, 3.8, 3.0]
+    ])
+
+    # Centroids, and centroid-color mapping
+    Mu = {
+        1: [6.2, 3.2],
+        2: [6.6, 3.7],
+        3: [6.5, 3.0]
+    }
+    Mu_color = {1: 'r', 2: 'g', 3: 'b'}
+
+    # Initial Plot : No Classification
+    # _show_plot(points, Mu, Mu_color)
+
+    # Kmeans Classification, points according to the initial centroids
+    closest, color_map = _assignment(points, Mu, Mu_color)
+    print('Classification Vector (First Iteration): ',
+          np.vstack([points, color_map]))
+    _show_plot(points, Mu, Mu_color, color_map=color_map,
+               filename='task3_iter1_a.png')
+
+    # Update Centroids
+    Mu = _update_Mu(Mu, closest, points)
+    print('Updated Centroids : ', Mu)
+    _show_plot(points, Mu, Mu_color, color_map=color_map,
+               _scatter=False, filename='task3_iter1_b.png')
+
+    # Kmeans Classification second iteration
+    # Recompute coloring based on new centroids
+    closest, color_map = _assignment(points, Mu, Mu_color)
+    print('Classification Vector (Second Iteration) : ',
+          np.vstack([points, color_map]))
+    _show_plot(points, Mu, Mu_color, color_map=color_map,
+               filename='task3_iter2_a.png')
+
+    # Update Centroids for the second time
+    Mu = _update_Mu(Mu, closest, points)
+    print('Updated Centroids : ', Mu)
+    _show_plot(points, Mu, Mu_color, color_map=color_map,
+               _scatter=False, filename='task3_iter2_b.png')
